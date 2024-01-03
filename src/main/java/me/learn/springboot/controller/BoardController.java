d82156c755a93642b3fe48498d4b4686f413a647
codurfer <codurfer@gmail.com>
2024-01-03 14:58:41 +0900

    Read API

--- src/main/java/me/learn/springboot/controller/BoardController.java
+++ src/main/java/me/learn/springboot/controller/BoardController.java
@@ -3,23 +3,47 @@ package me.learn.springboot.controller;
 import lombok.RequiredArgsConstructor;
 import me.learn.springboot.domain.Article;
 import me.learn.springboot.dto.AddArticleRequest;
+import me.learn.springboot.dto.ArticleResponse;
 import me.learn.springboot.service.BoardService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController //HTTP Response Body에 객체 JSON 형식으로 반환하는 컨트롤러
 public class BoardController {
     private final BoardService boardService;
 
+    // HTTP 메서드가 POST일 때 전달받은 URL와 동일하면 메서드로 매핑
     @PostMapping("/test/data")
+    // 요청 본문 값 매핑
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = boardService.save(request);
 
+        // 요청한 자원이 성공적으로 생성되었으며 저장된 게시판 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/board")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = boardService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/board/{id}")
+    // URL 경로에서 값을 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = boardService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
--- src/main/java/me/learn/springboot/service/BoardService.java
+++ src/main/java/me/learn/springboot/service/BoardService.java
@@ -6,13 +6,26 @@ import me.learn.springboot.dto.AddArticleRequest;
 import me.learn.springboot.repository.BoardRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service // 빈으로 등록
 public class BoardService {
     private final BoardRepository boardRepository;
 
-    //블로그 글 추가 메서드
+    // 게시판 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return boardRepository.save(request.toEntity());
+    }
+
+    // 게시판 글 모두 조회
+    public List<Article> findAll() {
+        return boardRepository.findAll();
+    }
+
+    // 게시판 글 한개 조회
+    public Article findById(long id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("id "+id + "을 찾을 수 없습니다."));
     }
 }
